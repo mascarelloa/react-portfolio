a59e54df4b8a468b3b9f383afd97bf9504786b4f
mascarelloa <amfm0793@gmail.com>
2021-05-28 20:09:40 -0400

    updating portfolio.

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,89 @@
+import styled from "styled-components";
+import { Link as LinkRouter } from "react-router-dom";
+import { Link as LinkScroll } from "react-scroll";
+
+export const Nav = styled.nav`
+  background: ${({scrollNav}) =>(scrollNav ? '#000000' : "transparent")};
+  height: 90px;
+  width: auto;
+  display: flex;
+  align-items: center;
+  top: 0;
+  margin-top: -91px;
+  position: sticky;
+  padding-left: 5%;
+  padding-right: 5%;
+  z-index: 10;
+  transition: 0.8s all ease;
+
+  @media screen and (max-width: 960px) {
+    transition: 0.8s all ease;
+  }
+`;
+export const NavContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 90px;
+  width: 100%;
+  z-index: 1;
+`;
+
+export const NavLogo = styled(LinkRouter)`
+  color: #fff;
+  font-family:poppins;
+  font-weight:600;
+  cursor: pointer;
+  font-size:2.5rem;
+  align-items: center;
+  text-decoration: none;
+  text-transform: uppercase;
+`;
+
+export const MobileIcon = styled.div`
+  display: none;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 50px;
+    right: 0;
+    transform: translate(-100%, -60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+    color: #ffffff;
+  }
+`;
+
+export const NavMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  text-align: center;
+ 
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+export const NavItem = styled.li`
+  height: 90px;
+`;
+export const NavLinks = styled(LinkScroll)`
+  color: #fff;
+  font-weight:600;
+  letter-spacing:1px;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  text-transform: uppercase;
+  font-size: 1.1rem;
+  font-family: poppins;
+
+  &.active {
+    border-bottom: 3px solid #111111;
+  }
+`;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from "react";
+import {
+  Nav,
+  NavContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+} from "./Navbar";
+import { FaBars } from "react-icons/fa";
+
+const Navbar = ({toggle}) => {
+  const [scrollNav, setScrollNav] = useState(false)
+
+  const changeNav = () => {
+    if(window.scrollY >= 80) {
+      setScrollNav(true)
+    } else {
+      setScrollNav(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeNav)
+  })
+
+  return (
+    <>
+      <Nav scrollNav={scrollNav}>
+        <NavContainer>
+          <NavLogo to="/"> am.</NavLogo>
+          <MobileIcon onClick={toggle}>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About.</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="projects">Projects.</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="contact">Contact.</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="#">Resume.</NavLinks>
+            </NavItem>
+          </NavMenu>
+
+        </NavContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
