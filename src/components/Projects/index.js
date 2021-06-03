d75727283ac814f1be2941237c24810c88cacea0
mascarelloa <amfm0793@gmail.com>
2021-06-02 23:30:19 -0400

    restyling project cards and updating images.

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -8,16 +8,17 @@ import {
   ProjectsP,
   ProjectsImg,
   ButtonContainer,
-  Bar,
- 
+  Bar
 } from "./Projects";
-import "../../App.css";
-import astroscope from "../../project1.png"
-import shelfi from "../../shelfi.png"
+// import "../../App.css";
+import astroscope from "../../astroscope.png"
+import knowsy from "../../knowsy.png"
+import shelfi from "../../shelfi2.png"
 import placeholder from "../../placeholder.png"
 import jsquiz from "../../jsquiz.png"
-import weatherdash from "../../visual1.png"
-import passwordgenerator from "../../project4.png"
+// import weatherdash from "../../weatherdash.png"
+
+import "./Projects.css"
 
 const Projects = () => {
   return (
@@ -28,91 +29,42 @@ const Projects = () => {
           <Bar />
         </ProjectsH1 >
 
-        <ProjectsWrapper>
-        <ProjectsCard>
-                    <ProjectsImg>
-                    <img src={astroscope}/></ProjectsImg>
-                    <ProjectsH2>
-                        Astroscope
-                    </ProjectsH2>
-                    <ProjectsP>
-                    A web app that shows the user's daily horoscope along with NASA's astronomy photo of the day.
-                    </ProjectsP>
-                    <ButtonContainer>
-                        <a href="https://github.com/mascarelloa/astroscope" target="_blank" type="button" className="project-button">Repo</a>
-                        <a href="https://mascarelloa.github.io/astroscope/" target="_blank" type="button" className="project-button">Demo</a>
-                    </ButtonContainer>
-                    
-                </ProjectsCard>
+<div className="projects">
+        <div class="project-card">
+  <img src={astroscope} alt="Astroscope" />
+  <div className="project-caption">
+    <h2>Astroscope<span>HTML / CSS / JavaScript / Photoshop</span></h2>
+    <p>A web app that shows the user's daily horoscope along with NASA's astronomy photo of the day.</p><a href="#" class="project-button">Demo</a><a href="#" class="project-button">Repo</a>
+  </div>
+</div>
 
-                <ProjectsCard>
-                    <img src={shelfi}/>
-                    <ProjectsH2>
-                        Shelfi
-                    </ProjectsH2>
-                    <ProjectsP>
-                    A search engine app for book lovers to find their next good read and view book details.
-                    </ProjectsP>
-                    <ButtonContainer>
-                        <a href="https://mascarelloa.github.io/shelfi/" target="_blank" type="button" className="project-button">Repo</a>
-                        <a href="https://myshelfi.herokuapp.com/" target="_blank" type="button" className="project-button">Demo</a>
-                    </ButtonContainer>
-                </ProjectsCard>
+<div class="project-card">
+  <img src={knowsy} alt="Knowsy" />
+  <div className="project-caption">
+    <h2>Knowsy<span>React / MongoDB / Node / Express</span></h2>
+    <p>A web app that lets users take,create, and share themed quizzes for any occasion.</p><a href="#" class="project-button">Demo</a><a href="#" class="project-button">Repo</a>
+  </div>
+</div>
 
-                <ProjectsCard>
-                    <img src={placeholder}/>
-                    <ProjectsH2>
-                        Knowsy
-                    </ProjectsH2>
-                    <ProjectsP>
-                        A web app that lets users take,create, and share themed quizzes for any occasion.
-                    </ProjectsP>
-                    <ButtonContainer>
-                        <a href="" target="_blank" type="button" className="project-button">Repo</a>
-                        <a href="" target="_blank" type="button" className="project-button">Demo</a>
-                    </ButtonContainer>
-                </ProjectsCard>
-                <ProjectsCard>
-                    <img src={weatherdash}/>
-                    <ProjectsH2>
-                       Weather Dash
-                    </ProjectsH2>
-                    <ProjectsP>
-                    An app that displays the current weather and forecast for the next five days for any city.
-                    </ProjectsP>
-                    <ButtonContainer>
-                        <a href="https://github.com/mascarelloa/weather-dashboard" target="_blank" type="button" className="project-button">Repo</a>
-                        <a href="https://mascarelloa.github.io/weather-dashboard/" target="_blank" type="button" className="project-button">Demo</a>
-                    </ButtonContainer>
-                </ProjectsCard>
-                <ProjectsCard>
-                    <img src={jsquiz}/>
-                    <ProjectsH2>
-                       JavaScript Quiz
-                    </ProjectsH2>
-                    <ProjectsP>
-                    A fun, timed quiz that tests beginner developers' knowledge on javascript concepts and records.
-                    </ProjectsP>
-                    <ButtonContainer>
-                        <a href="https://github.com/mascarelloa/code-quiz" target="_blank" type="button" className="project-button">Repo</a>
-                        <a href="https://mascarelloa.github.io/code-quiz/" target="_blank" type="button" className="project-button">Demo</a>
-                    </ButtonContainer>
-                </ProjectsCard>
+<div class="project-card">
+  <img src={shelfi} alt="Shelfi" />
+  <div className="project-caption">
+    <h2>Shelfi<span>HTML / CSS / JavaScript / Node / MySQL</span></h2>
+    <p>A search engine app for book lovers to find their next good read and view book details.</p><a href="#" class="project-button">Demo</a><a href="#" class="project-button">Repo</a>
+  </div>
+</div>
 
-                <ProjectsCard>
-                    <img src={passwordgenerator}/>
-                    <ProjectsH2>
-                       Password Generator
-                    </ProjectsH2>
-                    <ProjectsP>
-                    An app that generates a secure, strong password based on user's selected conditions.
-                    </ProjectsP>
-                    <ButtonContainer>
-                        <a href="https://github.com/mascarelloa/password-generator" target="_blank" type="button" className="project-button">Repo</a>
-                        <a href="https://mascarelloa.github.io/password-generator/" target="_blank" type="button" className="project-button">Demo</a>
-                    </ButtonContainer>
-                </ProjectsCard>
-        </ProjectsWrapper>
+<div class="project-card">
+  <img src={jsquiz} alt="Shelfi" />
+  <div className="project-caption">
+    <h2>JS Quiz<span>HTML / CSS / JavaScript</span></h2>
+    <p>A fun, timed quiz that tests beginner developers' knowledge on javascript concepts and records.</p><a href="#" class="project-button">Demo</a><a href="#" class="project-button">Repo</a>
+  </div>
+</div>
+
+
+</div>
+        
       </ProjectsContainer>
     </>
   );
